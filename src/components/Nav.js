e306f9acbee9d12e485ab62c6819e7b742837eb9
MiriamPaternain <miriampaternain@gmail.com>
2023-11-24 11:20:40 +0100

    add nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,34 @@
+import logo from '../img/logoPortfolio.jpg';
+import { Link } from 'react-router-dom';
+
+function Nav() {
+  return (
+    <>
+      <nav className='mainNav'>
+        <div className='mainNav_logo'>
+          <a href='/'>
+            <img
+              src={logo}
+              alt='logo de la pagina'
+              className='mainNav_logo--img'
+            />
+          </a>
+        </div>
+        <div className='mainNav_links'>
+          <ul className='mainNav_links--ul'>
+            <li className='mainNav_links--ulli'>
+              <Link to='/about'>About</Link>
+            </li>
+            <li className='mainNav_links--ulli'>
+              <Link to='/works'>Works</Link>
+            </li>
+            <li className='mainNav_links--ulli'>
+              <Link to='/contact'>Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
+export default Nav;
